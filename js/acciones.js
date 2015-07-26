1d80c59d64f3cb5ab856a30ae75028c8e184317e
AL01F13 <fernanda.aguilarj@gmail.com>
2015-07-26 17:55:58 -0500

    cuatro
    js

---
+++ js/acciones.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	
+	$('#btnprincipal').on('tap',function(){
+	navigator.vibrate(400);
+	});
+	
+	$('#btntipos').on('tap',function(){
+	navigator.vibrate(400);
+	});
+	
+	$('#btnfactores').on('tap',function(){
+	navigator.vibrate(400);
+	});
+	
+	$('#btntest').on('tap',function(){
+	navigator.vibrate(400);
+	});
+	
+	$('#btngaleria').on('tap',function(){
+	navigator.vibrate(400);
+	});
+	
+	
+}); 
+});
+
